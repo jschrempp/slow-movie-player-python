6b615f20f95155871194f5b50c1d2c31b84396bb
Cyber Alchemist <cyberalchemist@makeralchemy.com>
2023-12-03 19:22:45 -0800

    start movie again when it ends

--- slow-movie.py
+++ slow-movie.py
@@ -2,6 +2,10 @@ import cv2
 import pygame
 import time
 
+BLACK_RGB = (0, 0, 0)
+
+
+# Configuration settings
 MP4_FILE = "DOCTOR_WHO_2_STATE_OF_DECAY.mp4" 
 DELAY_BETWEEN_FRAMES = 1
 FRAMES_INCREMENT = 10
@@ -69,69 +73,86 @@ screen_width = display_info.current_w
 screen_height = display_info.current_h
 print(f'Screen width: {screen_width} height: {screen_height}')
 
-# Loop to extract and display the frames
-while frame <= total_frames:
 
-    # Check to see if the user wants to quit
-    stop = False
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            stop = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+# Loop forever - when the movie ends, start it again. Pressing ESC will stop
+total_frame = 1 # set up for just the first time through the playing loop
+frame = 1
+
+while True:
+
+    # Loop to extract and display the frames
+    while frame <= total_frames:
+
+        # Check to see if the user wants to quit
+        stop = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 stop = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    stop = True
+        if stop:
+            break
+
+        # Extract the frame
+        print(f"Extracting frame {frame}...")
+        total_frames = extract_frame(MP4_FILE, frame, "output_frame.jpg")
+
+        # Display the frame
+        image = pygame.image.load('output_frame.jpg')
+
+        if SCALE_IMAGE:
+
+            # Calculate the aspect ratio of the image and the screen
+            image_aspect_ratio = image.get_width() / image.get_height()
+            screen_aspect_ratio = screen_width / screen_height
+            print(f"Screen aspect ratio: {screen_aspect_ratio}, Image aspect ratio: {image_aspect_ratio}")
+
+            # Determine the scaling factor
+            if image_aspect_ratio > screen_aspect_ratio:
+                # Fit by width
+                print("Fit by width")
+                scale_factor = screen_width / image.get_width()
+            else:
+                # Fit by height
+                print("Fit by height")
+                scale_factor = screen_height / image.get_height()
+
+            # Scale the image to fit the screen
+
+            scaled_width = int(image.get_width()) * scale_factor
+            scaled_height = int(image.get_height()) * scale_factor
+
+            print(f"*** image width: {image.get_width()} height: {image.get_height()}")
+
+            scaled_image = pygame.transform.scale(image, (scaled_width, scaled_height))
+
+            print(f"*** scaled_image width: {scaled_width} height: {scaled_height}")
+
+            # With the image centered, make sure the sides are black
+            screen.fill(BLACK_RGB)
+
+            # Determine where to place the scaled image so it's centered on the screen
+            centered_width_position = int((screen_width - scaled_width)/2)
+
+            # Blit the scaled image onto the screen surface
+            screen.blit(scaled_image, (centered_width_position, 0))
+
+        else:
+            # for debugging
+            screen.fill((255, 0, 0))
+
+            screen.blit(image, (0,0))
+
+        pygame.display.flip()
+
+        frame += FRAMES_INCREMENT
+
+        # wait before displaying the next frame
+        time.sleep(DELAY_BETWEEN_FRAMES)
+
     if stop:
         break
-    
-    # Extract the frame
-    print(f"Extracting frame {frame}...")
-    total_frames = extract_frame(MP4_FILE, frame, "output_frame.jpg")
-
-    # Diaplay the frame
-    image = pygame.image.load('output_frame.jpg')
-
-    if SCALE_IMAGE:
-
-        # Calculate the aspect ratio of the image and the screen
-        image_aspect_ratio = image.get_width() / image.get_height()
-        screen_aspect_ratio = screen_width / screen_height
-        print(f"Screen aspect ratio: {screen_aspect_ratio}, Image aspect ratio: {image_aspect_ratio}")
-
-        # Determine the scaling factor
-        if image_aspect_ratio > screen_aspect_ratio:     
-            # Fit by width
-            print("Fit by width")
-            scale_factor = screen_width / image.get_width()
-        else:
-            # Fit by height
-            print("Fit by height")
-            scale_factor = screen_height / image.get_height()
-
-        # Scale the image to fit the screen
-        print(f"*** image width: {image.get_width()} * {scale_factor} height: {image.get_height()} * {scale_factor}")
-
-        scaled_image = pygame.transform.scale(image, (int(image.get_width() * scale_factor), int(image.get_height() * scale_factor)))
-
-        print(f"*** scaled_image width: {scaled_image.get_width()} height: {scaled_image.get_height()}")
-
-        # for debugging
-        screen.fill((255, 0, 0))
-
-        # Blit the scaled image onto the screen surface
-        screen.blit(scaled_image, (0, 0))
-
-    else:
-        # for debugging
-        screen.fill((255, 0, 0))
-
-        screen.blit(image, (0,0))
-
-    pygame.display.flip()
-
-    frame += FRAMES_INCREMENT    
-
-    # wait before displaying the next frame
-    time.sleep(DELAY_BETWEEN_FRAMES)
 
 pygame.quit()
 
