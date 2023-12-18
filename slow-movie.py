dba554ad6861e23e89462ce5a9ccedf62abc0700
Cyber Alchemist <cyberalchemist@makeralchemy.com>
2023-12-18 11:53:13 -0800

    added command line parsing for the playing configuration

--- slow-movie.py
+++ slow-movie.py
@@ -1,3 +1,4 @@
+import argparse
 import cv2
 import os
 import pygame
@@ -6,13 +7,6 @@ import time
 
 BLACK_RGB = (0, 0, 0)
 RED_RGB   = (255, 0, 0)
-
-# Configuration settings
-MP4_FILE = "CORPSE_BRIDE.mp4" 
-DELAY_BETWEEN_FRAMES = 1
-FRAMES_INCREMENT = 10
-SCALE_IMAGE = True
-DEBUG = True
 
 def calculate_time_to_play(number_of_frames, time_between_frames, frames_per_iteration):
     seconds = (number_of_frames * time_between_frames) / frames_per_iteration
@@ -29,10 +23,7 @@ def calculate_time_to_play(number_of_frames, time_between_frames, frames_per_ite
         return minutes, "minutes"
     else:
         return seconds, "seconds"
-    
-    
-    
-    return playing_time_seconds, playing_time_minutes, playing_time_hours, playing_time_days
+
 
 def extract_frame(video_filename, frame_number, output_filename):
     # Open the video file
@@ -64,15 +55,14 @@ def extract_frame(video_filename, frame_number, output_filename):
 
     # Save the frame to the specified output file
     cv2.imwrite(output_filename, frame)
-
-    frame_aspect_ratio = frame.shape[1] / frame.shape[0]
-    if DEBUG:
-        print(f"frame height={frame.shape[0]} width={frame.shape[1]} aspect ratio={frame_aspect_ratio}")
-
+    
     # Release the video file
     cap.release()
 
-    if DEBUG:
+    frame_aspect_ratio = frame.shape[1] / frame.shape[0]
+    
+    if debug:
+        print(f"frame height={frame.shape[0]} width={frame.shape[1]} aspect ratio={frame_aspect_ratio}")
         print(f"Frame {frame_number} extracted and saved to {output_filename}.")
 
     return total_frames
@@ -101,10 +91,50 @@ def add_text_to_image(image, left_text, right_text, font_size=20, text_color=(25
     # Blit the text onto the image
     image.blit(text_surface_left, text_rect_left)
     image.blit(text_surface_right, text_rect_right)
-    
-# exit if the file to play can't be found
-if not(os.path.exists(MP4_FILE)):
-    print(f"{MP4_FILE} can not be found!")
+
+
+# main execution starts here
+
+# get the configuration from the command line parameters
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="file name of movie to play")
+parser.add_argument("-d", "--delay", type=int, default=1
+    ,help="delay between frames in seconds")
+parser.add_argument("-f", "--frames_increment", default=1
+    ,help='frame increment (frame=1 means play every frame, frame=10 means play every 10 frames)')
+parser.add_argument("-n", "--no_scale", action="store_false"
+    ,help="Do not scale movie frames to fit display") 
+parser.add_argument("-x", "--debug", action="store_true"
+    ,help="Display debug messages")
+parser.add_argument("-t", "--test_mode", action="store_true"
+    ,help="Test mode: delay between frames: 1 second; frame increment: 10; scale image; debug mode on")
+args = parser.parse_args()
+
+mp4_file = args.filename
+delay_between_frames = args.delay
+frames_increment = args.frames_increment
+scale_image = args.no_scale
+debug = args.debug
+test_mode = args.test_mode
+
+# If test mode was specified, override the parameters to the test mode settings
+if test_mode:
+    delay_between_frames = 1
+    frames_increment = 10
+    scale_image = True 
+    debug = True 
+
+if debug:
+    print(f"mp4_file={mp4_file}")
+    print(f"delay_between_frames={delay_between_frames}")
+    print(f"frames_increment={frames_increment}")
+    print(f"scale_image={scale_image}")
+    print(f"debug={debug}")
+
+# Exit if the file to play can't be found
+if not(os.path.exists(mp4_file)):
+    print(f"{mp4_file} can not be found!")
     sys.exit()
 
 # Initialize PyGame
@@ -123,14 +153,14 @@ print(f'Screen width: {screen_width} height: {screen_height}')
 # Count the number of times the movie has been played
 movie_played = 0
 
-# Loop forever - when the movie ends, start it again. Pressing ESC will stop
+# Loop forever - when the movie ends, start it again. Pressing ESC will stop the movie player
 while True:
     # Initialize the loop variables
     total_frames = 1   # set up for just the first time through the movie playing loop
     frame = 1
     
     movie_played += 1
-    print(f"Playing {MP4_FILE}. Iteration {movie_played}.")
+    print(f"Playing {mp4_file}. Iteration {movie_played}.")
 
     # Loop to extract and display the frames
     while frame <= total_frames:
@@ -149,22 +179,21 @@ while True:
             break
 
         # Extract the frame
-        total_frames = extract_frame(MP4_FILE, frame, "output_frame.jpg")
+        total_frames = extract_frame(mp4_file, frame, "output_frame.jpg")
         # Print the playing time once
         if frame == 1:
-            duration, duration_units = calculate_time_to_play(total_frames, DELAY_BETWEEN_FRAMES, FRAMES_INCREMENT)
+            duration, duration_units = calculate_time_to_play(total_frames, delay_between_frames, frames_increment)
             playing_time = f"{duration:,.2f} {duration_units}"
             print(f"Time to play: {playing_time}")
         # Construct the status message
         percent_played = int((frame / total_frames) * 100)
         frame_message = f"Playback {movie_played:,} Frame {frame:,} of {total_frames:,} ({percent_played}%)"
-        print(MP4_FILE, frame_message)
+        print(mp4_file, frame_message)
 
         # Display the frame
         image = pygame.image.load('output_frame.jpg')
         
-        if SCALE_IMAGE:
-
+        if scale_image:
             # Calculate the aspect ratio of the image and the screen
             image_aspect_ratio = image.get_width() / image.get_height()
             screen_aspect_ratio = screen_width / screen_height
@@ -184,18 +213,14 @@ while True:
                 scale_factor = screen_height / image.get_height()
 
             # Scale the image to fit the screen
-
             scaled_width = int(image.get_width()) * scale_factor
             scaled_height = int(image.get_height()) * scale_factor
-
-            if DEBUG:
-                print(f"Image width: {image.get_width()} height: {image.get_height()}")
-
             scaled_image = pygame.transform.scale(image, (scaled_width, scaled_height))
-            
-            if DEBUG:
+
+            if debug:
+                print(f"Image width: {image.get_width()} height: {image.get_height()}")
                 print(f"Scaled_image width: {scaled_width} height: {scaled_height}")
-                file_info = f"{MP4_FILE} ({playing_time})"
+                file_info = f"{mp4_file} ({playing_time})"
                 add_text_to_image(scaled_image, file_info, frame_message)
 
             # Determine where to place the scaled image so it's centered on the screen
@@ -207,7 +232,7 @@ while True:
             # Blit the scaled image onto the screen surface
             screen.blit(scaled_image, (centered_width_position, 0))
         
-        # SCALED_IMAGE = False
+        # scaled_image = False
         else:
             # fill screen to red for debugging
             screen.fill(RED_RGB)
@@ -217,10 +242,10 @@ while True:
 
         pygame.display.flip()
 
-        frame += FRAMES_INCREMENT
+        frame += frames_increment
 
         # wait before displaying the next frame
-        time.sleep(DELAY_BETWEEN_FRAMES)
+        time.sleep(delay_between_frames)
 
     # If the user pressed ESC exit the forever loop to stop the program
     if stop:
